99ab27b437bc51e2d30787c6ae4b4f6d7daf2d28
kailyncleere <79721722+kailyncleere@users.noreply.github.com>
2025-04-01 23:00:23 -0500

    setup homepage for navigation
    broke input field/button?

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import ToDoForm from './components/ToDoForm';
 import ToDoList from './components/ToDoList';
+import Navbar from './navbar';
+import Contact from './contact';
 
 function App() {
+let component
+  switch (window.location.pathname) {
+    case '/':
+      component = <ToDoList />;
+      break;
+    case '/contact':
+      component = <Contact />;
+      break;
+    default:
+      component = <ToDoList />;
+  }
   return (
     <div className="body">
+      {}
+      <Navbar />
     <div className="Container">
-    <ToDoList />
+    {component} {}
     </div>
     </div>
   );
 }
 
 export default App;
+
